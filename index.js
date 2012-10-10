c496aaf81ba53e9c99299b701b4034edbea1b928
Sangmin Yoon <sangmin.yoon@croquis.com>
2012-10-10 12:25:36 +0900

    Exports types in the main module

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 exports.DBConnection = process.env.TEST_COV
   ? require('./lib-cov/connection')
   : require('./lib/connection')
+
+types = require('./lib/types');
+for (type in types) {
+  exports[type] = types[type]
+}
