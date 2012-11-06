829be034f650bafc925644240bde298fe6989534
Sangmin Yoon <sangmin.yoon@croquis.com>
2012-11-06 13:35:12 +0900

    Adds test case in JavaScript

---
+++ test/cases/javascript.js
@@ -0,0 +1,119 @@
+_createUsers = function (User, data, callback) {
+  if (typeof(data)==='function') {
+    callback = data;
+    data = [
+      { name: 'John Doe', age: 27 },
+      { name: 'Bill Smith', age: 45 },
+      { name: 'Alice Jackson', age: 27 },
+      { name: 'Gina Baker', age: 32 },
+      { name: 'Daniel Smith', age: 53 }
+    ];
+  }
+  data.sort(function () { return 0.5 - Math.random(); }); // random sort
+  async.map(data, function (item, callback) {
+      User.create(item, callback);
+    }, callback);
+};
+
+module.exports = function (models) {
+  it('create one', function (done) {
+    var user = new models.User();
+    user.name = 'John Doe';
+    user.age = 27;
+    user.should.have.property('name', 'John Doe');
+    user.should.have.property('age', 27);
+    done(null);
+  });
+
+  it('initialize in constructor', function (done) {
+    var user = new models.User({name: 'John Doe', age: 27});
+    user.should.have.property('name', 'John Doe');
+    user.should.have.property('age', 27);
+    done(null);
+  });
+
+  it('build method', function (done) {
+    var user = models.User.build({name: 'John Doe', age: 27});
+    user.should.have.property('name', 'John Doe');
+    user.should.have.property('age', 27);
+    done(null);
+  });
+
+  it('add a new record to the database', function (done) {
+    var user = new models.User({name: 'John Doe', age: 27});
+    user.save(function (error) {
+      if (error) {
+        return done(error);
+      }
+      user.should.have.property('id');
+      done(null);
+    });
+  });
+
+  it('create method', function (done) {
+    models.User.create({ name: 'John Doe', age: 27 }, function (error, user) {
+      if (error) {
+        return done(error);
+      }
+      user.should.have.property('id');
+      done(null);
+    });
+  });
+
+  it('simple where', function (done) {
+    _createUsers(models.User, function (error, users) {
+      if (error) {
+        return done(error);
+      }
+      models.User.where({age: 27}, function (error, users) {
+        if (error) {
+          return done(error);
+        }
+        users.should.have.length(2)
+        users.sort(function (a, b) { return a.name < b.name ? -1 : 1; });
+        users[0].should.have.property('name', 'Alice Jackson');
+        users[0].should.have.property('age', 27);
+        users[1].should.have.property('name', 'John Doe');
+        users[1].should.have.property('age', 27);
+        done(null);
+      });
+    });
+  });
+
+  it('where chain', function (done) {
+    _createUsers(models.User, function (error, users) {
+      if (error) {
+        return done(error);
+      }
+      models.User.where({age: 27}).where({name: 'Alice Jackson'}).exec(function (error, users) {
+        if (error) {
+          return done(error);
+        }
+        users.should.have.length(1);
+        users[0].should.have.property('name', 'Alice Jackson');
+        users[0].should.have.property('age', 27);
+        done(null);
+      });
+    });
+  });
+
+  it('$or', function (done) {
+    _createUsers(models.User, function (error, users) {
+      if (error) {
+        return done(error);
+      }
+      models.User.where({$or: [ { age: 32 }, { name: 'John Doe' } ]}, function (error, users) {
+        if (error) {
+          return done(error);
+        }
+        users.should.have.length(2);
+        users.sort(function (a, b) { return a.name < b.name ? -1 : 1; });
+        users[0].should.have.property('name', 'Gina Baker');
+        users[0].should.have.property('age', 32);
+        users[1].should.have.property('name', 'John Doe');
+        users[1].should.have.property('age', 27);
+        done(null);
+      });
+    });
+  });
+};
---
+++ test/javascript.js
@@ -0,0 +1,64 @@
+require('./common');
+
+_dbs = {
+  mysql: { database: 'test' },
+  mongodb: { database: 'test' },
+  sqlite3: { database: __dirname + '/test.sqlite3' },
+  sqlite3_memory: {},
+  postgresql: { database: 'test' }
+};
+Object.keys(_dbs).forEach(function (db) {
+  describe('javascript-' + db, function () {
+    var connection = undefined;
+    var connect = function (callback) {
+      connection = new Connection(db, _dbs[db]);
+      if (connection.connected) {
+        callback();
+      } else {
+        connection.once('connected', callback);
+        connection.once('error', function (error) {
+          callback(error);
+        });
+      }
+    };
+
+    var models = {}
+
+    before(function (done) {
+      connect(function (error) {
+        if (error) {
+          return done(error);
+        }
+
+        var User = models.User = connection.model('User', { name: String, age: Number });
+
+        User.drop(function (error) {
+          if (error) {
+            return done(error);
+          }
+          connection.applySchemas(function (error) {
+            if (error) {
+              return done(error);
+            }
+            done(null);
+          });
+        });
+      });
+    });
+
+    beforeEach(function (done) {
+      models.User.deleteAll(function (error) {
+        if (error) {
+          return done(error);
+        }
+        done(null);
+      });
+    });
+
+    after(function (done) {
+      models.User.drop(done);
+    });
+
+    require('./cases/javascript')(models);
+  });
+});
