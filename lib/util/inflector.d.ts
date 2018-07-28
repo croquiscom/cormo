d3cff583819bd5fb106483c37b333816f07e0285
Sangmin Yoon <sangmin.yoon@croquis.com>
2018-07-28 13:53:04 +0900

    util/inflector

--- lib/util/inflector.d.ts
+++ lib/util/inflector.d.ts
@@ -1,1 +1,35 @@
-declare var inflect: any;
+/**
+ * Returns foreign_key for a name
+ * @memberOf inflector
+ */
+export declare function foreign_key(name: string): string;
+/**
+ * Returns pluralized string of a string
+ * @memberOf inflector
+ */
+export declare function pluralize(str: string): string;
+/**
+ * Returns singularized string of a string
+ * @memberOf inflector
+ */
+export declare function singularize(str: string): string;
+/**
+ * Returns table name of a name
+ * @memberOf inflector
+ */
+export declare function tableize(name: string): string;
+/**
+ * Returns class name of a name
+ * @memberOf inflector
+ */
+export declare function classify(name: string): string;
+/**
+ * Returns underscored string of a string
+ * @memberOf inflector
+ */
+export declare function underscore(str: string): string;
+/**
+ * Returns camelized string of a string
+ * @memberOf inflector
+ */
+export declare function camelize(str: string): string;
