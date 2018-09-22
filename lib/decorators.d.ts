41ad1c32de054c90f151ca4b69d5e9e62562f122
Sangmin Yoon <sangmin.yoon@croquis.com>
2018-09-22 21:52:54 +0900

    add HasMany, HasOne, BelongsTo decorator

--- lib/decorators.d.ts
+++ lib/decorators.d.ts
@@ -1,3 +1,7 @@
+import { IAssociationBelongsToOptions, IAssociationHasManyOptions, IAssociationHasOneOptions } from './connection';
 import { BaseModel, IColumnProperty } from './model';
 import * as types from './types';
 export declare function Column(column_property: IColumnProperty | types.ColumnType): (target: BaseModel, propertyKey: string) => void;
+export declare function HasMany(options?: IAssociationHasManyOptions): (target: BaseModel, propertyKey: string) => void;
+export declare function HasOne(options?: IAssociationHasOneOptions): (target: BaseModel, propertyKey: string) => void;
+export declare function BelongsTo(options?: IAssociationBelongsToOptions): (target: BaseModel, propertyKey: string) => void;
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,9 +1,38 @@
-import { BaseModel, IColumnProperty } from './model';
+import {
+  IAssociationBelongsToOptions,
+  IAssociationHasManyOptions,
+  IAssociationHasOneOptions,
+} from './connection';
+import {
+  BaseModel,
+  IColumnProperty,
+} from './model';
 import * as types from './types';
 
 export function Column(column_property: IColumnProperty | types.ColumnType) {
   return (target: BaseModel, propertyKey: string) => {
     const ctor = target.constructor as typeof BaseModel;
     ctor.column(propertyKey, column_property);
+  };
+}
+
+export function HasMany(options?: IAssociationHasManyOptions) {
+  return (target: BaseModel, propertyKey: string) => {
+    const ctor = target.constructor as typeof BaseModel;
+    ctor.hasMany(propertyKey, options);
+  };
+}
+
+export function HasOne(options?: IAssociationHasOneOptions) {
+  return (target: BaseModel, propertyKey: string) => {
+    const ctor = target.constructor as typeof BaseModel;
+    ctor.hasOne(propertyKey, options);
+  };
+}
+
+export function BelongsTo(options?: IAssociationBelongsToOptions) {
+  return (target: BaseModel, propertyKey: string) => {
+    const ctor = target.constructor as typeof BaseModel;
+    ctor.belongsTo(propertyKey, options);
   };
 }
