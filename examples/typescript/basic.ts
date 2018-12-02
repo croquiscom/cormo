30715ff020f3a946174ef8206c1d1329ad4917b1
Sangmin Yoon <sangmin.yoon@croquis.com>
2018-12-02 14:49:06 +0900

    introduce Name class to the TypeScript example

--- examples/typescript/basic.ts
+++ examples/typescript/basic.ts
@@ -1,3 +1,5 @@
+// tslint:disable:max-classes-per-file
+
 import * as cormo from '../../src';
 
 const connection = new cormo.Connection('mysql', {
@@ -7,50 +9,62 @@ const connection = new cormo.Connection('mysql', {
   user: 'cormo_test',
 });
 
+class Name {
+  public first!: string;
+  public last!: string;
+
+  public toString() {
+    return `${this.first} ${this.last}`;
+  }
+}
+
 @cormo.Model({ connection })
-@cormo.Index({ name: 1, age: 1 })
+@cormo.Index({ name_first: 1, age: 1 })
 class User extends cormo.BaseModel {
   public id!: number;
 
-  @cormo.Column({ type: String, required: true })
-  public name!: string;
+  @cormo.Column({
+    first: { type: String, required: true },
+    last: { type: String, required: true },
+  })
+  public name!: Name;
 
   @cormo.Column(Number)
   public age?: number;
 }
 
 async function create1() {
-  const user = await User.create({ name: 'croquis' });
-  console.log(user.name);
+  const user = await User.create({ name: { first: 'John', last: 'Doe' } });
+  console.log(Name.prototype.toString.apply(user.name));
 }
 
 async function create2() {
-  const user = await User.create({ name: 'foobar', age: 5 });
-  console.log(user.name);
+  const user = await User.create({ name: { first: 'Bill', last: 'Smith' }, age: 5 });
+  console.log(Name.prototype.toString.apply(user.name));
 }
 
 async function save() {
-  const user = await new User({ name: 'cormo' });
+  const user = await new User({ name: { first: 'Alice', last: 'Jackson' } });
   user.save();
-  console.log(user.name);
+  console.log(Name.prototype.toString.apply(user.name));
 }
 
 async function build() {
-  const user = await User.build({ name: 'rinore' });
+  const user = await User.build({ name: { first: 'Gina', last: 'Baker' } });
   await user.save();
-  console.log(user.name);
+  console.log(Name.prototype.toString.apply(user.name));
 }
 
 async function getAll() {
   const users = await User.where();
   for (const user of users) {
-    console.log(`name - ${user.name}, age - ${user.age}`);
+    console.log(`name - ${Name.prototype.toString.apply(user.name)}, age - ${user.age}`);
   }
 }
 
 async function query() {
-  const foobar = await User.where({ age: 5 }).select(['id', 'name']).one();
-  console.log(`name of age 5 is #${foobar.id} ${foobar.name}`);
+  const user = await User.where({ age: 5 }).select(['id', 'name']).one();
+  console.log(`name of age 5 is #${user.id} ${Name.prototype.toString.apply(user.name)}`);
 }
 
 async function count() {
@@ -71,5 +85,8 @@ async function run() {
 
 run().then(() => {
   console.log('Done');
+  process.exit(0);
+}).catch((error) => {
+  console.log((error.cause || error).toString());
   process.exit(0);
 });
