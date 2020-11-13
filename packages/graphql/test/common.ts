c34798bce415fb78b247491c60baf5c313d2b74b
Sangmin Yoon <sangmin.yoon@croquis.com>
2020-11-13 15:32:47 +0900

    remove travis

--- packages/graphql/test/common.ts
+++ packages/graphql/test/common.ts
@@ -1,25 +1,12 @@
-import * as cormo from 'cormo';
-
-let db_configs: { [db: string]: any };
-if (process.env.TRAVIS === 'true') {
-  db_configs = {
-    mysql: {
-      implicit_apply_schemas: true,
-      database: 'travis',
-      user: 'travis',
-    },
-  };
-} else {
-  db_configs = {
-    mysql: {
-      implicit_apply_schemas: true,
-      database: 'cormo_test',
-      password: 'cormo_test',
-      port: 21860,
-      user: 'cormo_test',
-    },
-  };
-}
+const db_configs: { [db: string]: any } = {
+  mysql: {
+    implicit_apply_schemas: true,
+    database: 'cormo_test',
+    password: 'cormo_test',
+    port: 21860,
+    user: 'cormo_test',
+  },
+};
 
 const _g = {
   db_configs,
