8ab272e84ce06f89cdb3ecee71a06812f73bb270
Sangmin Yoon <sixmen@gmail.com>
2024-10-27 08:23:42 +0900

    refactor: dynamic load esm modules for cjs build

--- packages/cormo/lib/esm/logger/ColorConsoleLogger.js
+++ packages/cormo/lib/esm/logger/ColorConsoleLogger.js
@@ -1,6 +1,18 @@
-import chalk from 'chalk';
+let chalk;
+import('chalk')
+    .then((m) => {
+    chalk = m.default;
+})
+    .catch(() => {
+    //
+});
 export class ColorConsoleLogger {
     logQuery(text, values) {
-        console.log('  ', chalk.blue.bold(text), values);
+        if (chalk) {
+            console.log('  ', chalk.blue.bold(text), values);
+        }
+        else {
+            console.log(text, values);
+        }
     }
 }
--- packages/cormo/src/logger/ColorConsoleLogger.ts
+++ packages/cormo/src/logger/ColorConsoleLogger.ts
@@ -1,8 +1,21 @@
-import chalk from 'chalk';
 import { Logger } from './Logger.js';
+
+let chalk: typeof import('chalk').default | undefined;
+
+import('chalk')
+  .then((m) => {
+    chalk = m.default;
+  })
+  .catch(() => {
+    //
+  });
 
 export class ColorConsoleLogger implements Logger {
   public logQuery(text: string, values?: any[]): void {
-    console.log('  ', chalk.blue.bold(text), values);
+    if (chalk) {
+      console.log('  ', chalk.blue.bold(text), values);
+    } else {
+      console.log(text, values);
+    }
   }
 }
